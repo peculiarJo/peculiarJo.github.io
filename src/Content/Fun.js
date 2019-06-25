413bd0ba0e04ff6e5715517d0adc24bc2e5d179e
Zhou Wu <Zhou.Wu@dentsuaegis.com>
2019-06-25 15:08:26 +0800

    add weiridThing

--- src/Content/Fun.js
+++ src/Content/Fun.js
@@ -40,6 +40,32 @@ class Fun extends React.Component {
                         </div>
                     </div>
                 </div>
+                <div className="container-fluid">
+                    <div className="story-box">
+                        <div className="services">
+                            <span className="icon text-center">
+                                <i className="fa fa-lightbulb-o fa-2x"></i>
+                            </span>
+                            <div className="story-body">
+                                <div className="card mb-3">
+                                    <div className="row no-gutters">
+                                        <div className="col-md-4" style={{maxHeight:"350px",overflowY:"scroll"}}>
+                                            <img src="/images/weirdThing.jpg" className="card-img" alt="..." />
+                                        </div>
+                                        <div className="col-md-8" style={{display:"flex",alignItems:"center"}}>
+                                            <div className="card-body">
+                                                <h3>2. WeirdThing - Community Portal</h3>
+                                                <p></p>
+                                                <p className="card-text">An interactive site hosted on firebase where users can register and then add posts and share with others</p>
+                                                <p className="card-text"><a href="https://my-react-project-32388.firebaseapp.com/">Check it out</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
         )
     }
